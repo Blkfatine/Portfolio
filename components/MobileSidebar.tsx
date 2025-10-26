b1db7559a8490841838c7645fd728887528698f2
Fatine Belkhammar <blkfatine2@gmail.com>
2025-10-26 20:37:10 +0100

    fix: add external property to Contact link in MobileSidebar

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -66,7 +66,8 @@ export default function MobileSidebar({ activeSection, onSectionChange }: Mobile
     { 
       href: '/contact', 
       label: 'Contact', 
-      icon: Mail 
+      icon: Mail,
+      external: true
     }
   ]
 
